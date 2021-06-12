236c65f8141fbf3c92fb957e04e873be5989b754
Renard <rdlafitte@gmail.com>
2021-06-11 21:07:42 -0300

    referencias

---
+++ 2doParcial_877071/2doParcial_877071.Datos/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2doParcial_877071.Datos
+{
+    public class Class1
+    {
+    }
+}
